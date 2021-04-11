d41e8cdb7d633da2d429b7d762c4858ead1a7197
Mohammed Al Farhan <alfarhanmah@gmail.com>
2021-04-11 15:13:07 +0300

    tiny fix to the exception class

--- include/hcore/exception.hh
+++ include/hcore/exception.hh
@@ -21,8 +21,12 @@ public:
     Error(const std::string& what_arg) : std::exception(), what_arg_(what_arg)
     {
     }
-    Error(const std::string& what_arg, const char* func) : std::exception(),
-        what_arg_(what_arg + " in function " + func)
+    Error(const std::string& what_arg,
+        const char* function, const char* file, int line) : std::exception(),
+        what_arg_(what_arg
+            + ", function " + function
+            + ", file " + file
+            + ", line " + std::to_string(line) + ".")
     {
     }
     virtual const char* what() const noexcept override
@@ -36,48 +40,58 @@ private:
 
 namespace internal {
 
-// internal helper function; throws Error if cond is true
-// called by hcore_error_if macro
-inline void throw_if(bool cond, const char* condstr, const char* func)
+// throws hcore::Error if condition is true called by hcore_error_if macro
+inline void throw_if(
+    bool condition, const char* condition_string,
+    const char* function, const char* file, int line)
 {
-    if (cond) {
-        throw Error(condstr, func);
+    if (condition) {
+        throw Error(condition_string, function, file, line);
     }
 }
-// internal helper function; throws Error if cond is true
-// uses printf-style format for error message
-// called by hcore_error_if_msg macro
-// condstr is ignored, but differentiates this from other version.
+// throws hcore::Error if condition is true called by hcore_error_if_msg macro
+// and uses printf-style format for error message
+// condition_string is ignored, but differentiates this from other version.
 inline void throw_if(
-    bool cond, const char* condstr, const char* func, const char* format, ...)
-    __attribute__((format(printf, 4, 5)));
+    bool condition, const char* condition_string,
+    const char* function, const char* file, int line, const char* format, ...)
+    __attribute__((format(printf, 6, 7)));
 
 inline void throw_if(
-    bool cond, const char* condstr, const char* func, const char* format, ...)
+    bool condition, const char* condition_string,
+    const char* function, const char* file, int line, const char* format, ...)
 {
-    if (cond) {
-        char buf[80];
-        va_list va;
-        va_start(va, format);
-        vsnprintf(buf, sizeof(buf), format, va);
-        throw Error(buf, func);
+    if (condition) {
+        char bufffer[80];
+        va_list v;
+        va_start(v, format);
+        vsnprintf(bufffer, sizeof(bufffer), format, v);
+        va_end(v);
+        throw Error(bufffer, function, file, line);
     }
 }
 
-// internal helper function; aborts if cond is true
+// internal helper function; aborts if condition is true
 // uses printf-style format for error message
 // called by hcore_error_if_msg macro
-inline void abort_if(bool cond, const char* func,  const char* format, ...)
-    __attribute__((format( printf, 3, 4 )));
+inline void abort_if(
+    bool condition,
+    const char* function, const char* file, int line, const char* format, ...)
+    __attribute__((format(printf, 5, 6)));
 
-inline void abort_if(bool cond, const char* func,  const char* format, ...)
+inline void abort_if(
+    bool condition,
+    const char* function, const char* file, int line, const char* format, ...)
 {
-    if (cond) {
-        char buf[80];
-        va_list va;
-        va_start(va, format);
-        vsnprintf(buf, sizeof(buf), format, va);
-        fprintf(stderr, "Error: %s, in function %s\n", buf, func);
+    if (condition) {
+        char bufffer[80];
+        va_list v;
+        va_start(v, format);
+        vsnprintf(bufffer, sizeof(bufffer), format, v);
+        va_end(v);
+        fprintf(stderr,
+            "HCORE assertion failed: (%s), function %s, file %s, line %d.\n",
+            bufffer, function, file, line);
         abort();
     }
 }
@@ -90,25 +104,29 @@ inline void abort_if(bool cond, const char* func,  const char* format, ...)
     // HCORE does no error checking, and thus errors maybe either handled by
     //     - BLAS++ and LAPACK++, or
     //     - Lower level BLAS and LAPACK via xerbla
-    #define hcore_error_if(cond)          ((void)0)
-    #define hcore_error_if_msg(cond, ...) ((void)0)
+    #define hcore_error_if(condition)          ((void)0)
+    #define hcore_error_if_msg(condition, ...) ((void)0)
 #elif defined(HCORE_ERROR_ASSERT)
     // HCORE aborts on error (similar to C/C++ assert)
-    #define hcore_error_if(cond) \
-        hcore::internal::abort_if(cond, __func__, "%s", #cond)
-    #define hcore_error_if_msg(cond, ...) \
-        hcore::internal::abort_if(cond, __func__, __VA_ARGS__)
+    #define hcore_error_if(condition) \
+        hcore::internal::abort_if( \
+            condition, __func__, __FILE__, __LINE__, "%s", #condition)
+    #define hcore_error_if_msg(condition, ...) \
+        hcore::internal::abort_if( \
+            condition, __func__, __FILE__, __LINE__, __VA_ARGS__)
 #else
     // HCORE throws errors (default)
-    // internal macro to get string #cond; throws Error if cond is true
-    // example: hcore_error_if(a < b)
-    #define hcore_error_if(cond) \
-        hcore::internal::throw_if(cond, #cond, __func__)
-    // internal macro takes cond and printf-style format for error message.
-    // throws Error if cond is true.
+    // internal macro to get string #condition; throws hcore::Error if condition
+    // is true. Example: hcore_error_if(a < b)
+    #define hcore_error_if(condition) \
+        hcore::internal::throw_if( \
+            condition, #condition, __func__, __FILE__, __LINE__)
+    // internal macro takes condition and printf-style format for error message.
+    // throws Error if condition is true.
     // example: hcore_error_if_msg(a < b, "a %d < b %d", a, b);
-    #define hcore_error_if_msg(cond, ...) \
-        hcore::internal::throw_if(cond, #cond, __func__, __VA_ARGS__)
+    #define hcore_error_if_msg(condition, ...) \
+        hcore::internal::throw_if( \
+            condition, #condition, __func__, __FILE__, __LINE__, __VA_ARGS__)
 #endif
 
 #endif // HCORE_EXCEPTION_HH
