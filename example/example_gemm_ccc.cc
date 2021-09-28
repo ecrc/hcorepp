5efa90c424f4981b142c28c955506db83e90d75a
Mohammed Al Farhan <alfarhanmah@gmail.com>
2021-09-28 16:51:31 +0300

    Update example_gemm_ccc.cc

--- example/example_gemm_ccc.cc
+++ example/example_gemm_ccc.cc
@@ -4,19 +4,12 @@
 // SPDX-License-Identifier: BSD-3-Clause. See the accompanying LICENSE file.
 
 #include <algorithm> // std::min
-#include <cstdint>   // int64_t
+#include <stdint.h>  // int64_t
 #include <stdio.h>   // printf
 #include <vector>    // std::vector
 
 #include "hcore/hcore.hh"
 
-// -----------------------------------------------------------------------------
-// Performs tile low-rank Matrix-matrix multiplication:
-// C = alpha * A * B + beta * C, where alpha and beta are scalars, and A, B, and
-// C are compressed matrices, with A an m-by-k tile low-rank matrix
-// (U: m-by-Ark, V: Ark-by-k), B a k-by-n tile low-rank matrix
-// (U: k-by-Brk, V: Brk-by-n), and C an m-by-n tile low-rank matrix
-// (U: m-by-Crk, V: Crk-by-n).
 template <typename T>
 void execute(int64_t m, int64_t n, int64_t k)
 {
@@ -51,22 +44,33 @@ void execute(int64_t m, int64_t n, int64_t k)
     //     auto AT = hcore::transpose(A);
     //     auto BT = hcore::conjugate_transpose(B);
     //     hcore::gemm<T>(alpha, AT, BT, beta, C);
+
+    // This is need to avoid memory leak because the C data array is resized
+    // inside gemm, when C is compressed
+    C.clear();
 }
 
+// -----------------------------------------------------------------------------
+// Performs tile low-rank Matrix-matrix multiplication:
+// C = alpha * A * B + beta * C, where alpha and beta are scalars, and A, B, and
+// C are compressed matrices, with A an m-by-k tile low-rank matrix
+// (U: m-by-Ark, V: Ark-by-k), B a k-by-n tile low-rank matrix
+// (U: k-by-Brk, V: Brk-by-n), and C an m-by-n tile low-rank matrix
+// (U: m-by-Crk, V: Crk-by-n).
 int main(int argc, char* argv[])
 {
     int64_t m = 100, n = 200, k = 50;
 
-    printf("execute<float>(%d, %d, %d)\n", m, n, k);
+    printf("execute<float>(%lld, %lld, %lld)\n", m, n, k);
     execute<float>(m, n, k);
 
-    printf("execute<double>(%d, %d, %d)\n", m, n, k);
+    printf("execute<double>(%lld, %lld, %lld)\n", m, n, k);
     execute<double>(m, n, k);
 
-    printf("execute<complex<float>>(%d, %d, %d)\n", m, n, k);
+    printf("execute<complex<float>>(%lld, %lld, %lld)\n", m, n, k);
     execute<std::complex<float>>(m, n, k);
 
-    printf("execute<complex<double>>(%d, %d, %d)\n", m, n, k);
+    printf("execute<complex<double>>(%lld, %lld, %lld)\n", m, n, k);
     execute<std::complex<double>>(m, n, k);
 
     return 0;
